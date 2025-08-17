ce96cfc3893ed31398bd60d1ac915a236affee5d
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-08-17 16:43:44 -0400

    [V.1.3.3] Fix: Errores de type en eventHandler

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ActionRowBudiler, ButtonBuidler, StringSelecMenuBuilder } = require("discord.js")
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder } = require("discord.js")
 const { readdirSync } = require("node:fs")
 
 module.exports = {
@@ -7,8 +7,8 @@ module.exports = {
 		readdirSync(process.cwd()+"/eventos").forEach((x) => {
 			readdirSync(process.cwd()+`/eventos/${x}`).filter(file => file.endsWith(".js")).forEach((y) => {
 				const event = require(process.cwd()+`/eventos/${x}/${y}`)
-				if(event.once) client.once(event.name, (...args) => event.execute(...args, client, EmbedBuilder, ActionRowBudiler, ButtonBuidler, StringSelecMenuBuilder)); else {
-					client.on(event.name, (...args) => event.execute(...args, client, EmbedBuilder, ActionRowBudiler, ButtonBuidler, StringSelecMenuBuilder))
+				if(event.once) client.once(event.name, (...args) => event.execute(...args, client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder)); else {
+					client.on(event.name, (...args) => event.execute(...args, client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder))
 				}
 			})
 		})
